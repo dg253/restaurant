47b84a87006ea07f91b214443722d17658061519
David <dg253@njit.edu>
2016-04-27 15:40:15 -0400

    -reservation date now a datepicker dropdown

--- js/materialize/init.js
+++ js/materialize/init.js
@@ -5,6 +5,10 @@
     $('.parallax').parallax();
     $('.slider').slider({});
     $('.carousel').carousel();
+    $('.datepicker').pickadate({
+       selectMonths: false, // Creates a dropdown to control month
+       selectYears: 1 // Creates a dropdown of 15 years to control year
+     });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
