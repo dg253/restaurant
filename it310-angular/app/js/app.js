1aca2f1c78ff6f971714546c45e6f3086c5dbf5c
David <dg253@njit.edu>
2016-04-28 20:05:47 -0400

    testing angular site

---
+++ it310-angular/app/js/app.js
@@ -0,0 +1,95 @@
+
+angular.module('IT310', ['ngResource', 'ui.router', 'ngCart'])
+
+.run (['$rootScope', '$state', function($rootScope, $state){
+    $rootScope.$state = $state;
+
+}])
+
+.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
+        if (window.location.port !== "8011"){
+            $locationProvider.html5Mode(true);
+        }
+
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+
+
+        .state('site', {
+            abstract:true,
+            url: "/",
+            controller:"main",
+            template: "<div ui-view></div>"
+        })
+
+
+        .state('site.index', {
+            url: "",
+            templateUrl: 'partials/index.html'
+        })
+
+      .state('site.cart', {
+            url: "cart",
+            controller:"cart",
+            templateUrl: 'partials/cart.html'
+        })
+
+        .state('site.docs', {
+            url: "docs",
+            templateUrl: 'partials/docs.html'
+        })
+
+
+}])
+    .controller('main',[ '$http','ngCart', '$scope', function ($http, ngCart, $scope) {
+
+        ngCart.setShipping(20);
+        ngCart.setTaxRate(7);
+
+
+    $http({method: 'GET', url: 'data/phones.json'})
+        .success(function(data, status, headers, config) {
+            $scope.products = data;
+        })
+        .error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+
+}])
+
+.controller('cart',['ngCart', '$log', '$scope', function (ngCart,$log, $scope) {
+
+
+        $scope.httpSettings = {
+            url:'/checkout'
+        };
+
+        $scope.payPalSettings ={ paypal:{
+            business:'dg3794@gmail.com',
+            item_name:'Order',
+            item_number:'item_number',
+            currency_code:'USD'
+        }};
+
+    $scope.showCart = function(){
+
+        $log.info ('---Total Cost:---');
+        $log.info (ngCart.totalCost());
+        $log.info ('---Items in Cart:---');
+        $log.info (ngCart.getItems());
+
+    }
+
+}])
+    .directive('rainbowBlock', function () {
+
+    return {
+        restrict: 'A',
+        link: function(el) {
+            Rainbow.color();
+        }
+    };
+})
+;
